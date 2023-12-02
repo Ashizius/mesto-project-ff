ead05cce624ea129d95c4b0d49ffcf1a58160426
Ashizius <Shishlov1989@yandex.ru>
2023-12-02 20:11:54 +0300

    add-remove-cards-v1

--- scripts/index.js
+++ scripts/index.js
@@ -3,26 +3,27 @@ const maxCards = 6; // максимальное число выводимых к
 const сardRandomizer = true; // включение перемешивания карточек
 let createdCards = []; // массив номеров выведенных карточек
 
-// @todo: Темплейт карточки
+// Темплейт карточки
 const cardTemplate = document.querySelector('#card-template').content;
 
-// @todo: DOM узлы
+// DOM узлы
 const placesList = document.querySelector('.places__list');
 
-// @todo: Функция создания карточки
+// Функция создания карточки
 const putCard = function (initialCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const cardImage = cardElement.querySelector('.card__image');
   const cardTitle = cardElement.querySelector('.card__title');
+  const cardDeleteButton = cardElement.querySelector('.card__delete-button');
   cardImage.src = initialCard.link;
-  cardImage.alt = initialCard.name;
+  cardImage.alt = 'фотография с изображением места' + initialCard.name;
   cardTitle.textContent = initialCard.name;
-  placesList.append(cardElement);
-  return 0;
+  cardDeleteButton.addEventListener('click',removeCard);
+  return cardElement;
 };
 
-function getRandomCard() {
-  //получить номер случайной карточки
+//получить номер случайной карточки
+const getRandomCard = function () {
   let rndCardNumber = Math.floor(Math.random() * (cardAmount - 1)); //случайный номер карточки
   //проверка на уникальность↓
   if (
@@ -34,16 +35,32 @@ function getRandomCard() {
   ) {
     rndCardNumber = getRandomCard(); //если уже есть в массиве, то взять другое случайное число
   } else {
-    createdCards.push(rndCardNumber); //записать в массив уникальных карточек
+    createdCards.push(rndCardNumber);
+    console.log(createdCards); //записать в массив уникальных карточек
   }
   //конец проверки на уникальность ↑
   return rndCardNumber;
 }
 
-// @todo: Функция удаления карточки
-const removeCard = function () {};
-// @todo: Вывести карточки на страницу
+// Функция удаления карточки
+const removeCard = function (event) {
+  createdCards=createdCards.filter(function (item) {
+      /*удалить!!!*/if (initialCards[item].name===event.target.parentElement.querySelector('.card__title').textContent) {console.log(item);console.log(event.target.parentElement.querySelector('.card__title').textContent);}
+      return initialCards[item].name!==event.target.parentElement.querySelector('.card__title').textContent
+    });
+  console.log(createdCards);
+  event.target.parentElement.remove();
+};
+// Вывести карточки на страницу
+initialCards.forEach(function (initialCard, index){
+  const card=сardRandomizer ? initialCards[getRandomCard()] : initialCard;
+  if (index < (cardAmount < maxCards ? cardAmount : maxCards)) {
+    placesList.append(putCard(card));
+  }
+});
 
-for (let i = 0; i < (cardAmount < maxCards ? cardAmount : maxCards); i++) {
-  putCard(сardRandomizer ? initialCards[getRandomCard()] : initialCards[i]);
-}
+/*for (let i = 0; i < (cardAmount < maxCards ? cardAmount : maxCards); i++) {
+  initialCard=сardRandomizer ? initialCards[getRandomCard()] : initialCards[i];
+  placesList.append(putCard(initialCard));
+  console.log(placesList);
+}*/
