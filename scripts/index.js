a9ef3df9a3b9c4b79b21f5e2102cb24271fed6d2
Ashizius <Shishlov1989@yandex.ru>
2023-12-02 22:23:59 +0300

    add-remove-cards-v4

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
-let cardAmount = initialCards.length + 1;
 const maxCards = 6; // максимальное число выводимых карточек
 const сardRandomizer = true; // включение перемешивания карточек
+let cardAmount = initialCards.length + 1;
 let createdCards = []; // массив выведенных на страницу карточек
 
 // Темплейт карточки
@@ -8,6 +8,7 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // DOM узлы
 const placesList = document.querySelector('.places__list');
+const placesListClone = placesList.cloneNode(false); //чтобы сразу все карточки добавить одним скопом в DOM
 
 // Функция размещения карточки
 const putCard = function (card, removeCard) {
@@ -16,7 +17,7 @@ const putCard = function (card, removeCard) {
   const cardTitle = cardElement.querySelector('.card__title');
   const cardDeleteButton = cardElement.querySelector('.card__delete-button');
   cardImage.src = card.link;
-  cardImage.alt = 'фотография с изображением места' + card.name;
+  cardImage.alt = 'фотография с изображением места ' + card.name;
   cardTitle.textContent = card.name;
   cardDeleteButton.addEventListener('click', removeCard);
   return cardElement;
@@ -27,9 +28,9 @@ const getRandomCard = function () {
   let randomCard = initialCards[Math.floor(Math.random() * (cardAmount - 1))]; //случайная карточка
   //проверка на уникальность ↓
   if (
-    createdCards.some(function (card) {
-      return randomCard.name === card.name && randomCard.link === card.link;
-    })
+    createdCards.some(
+      (card) => randomCard.name === card.name && randomCard.link === card.link
+    )
   ) {
     randomCard = getRandomCard(); //если уже есть в массиве, то взять другую случайную карточку
   }
@@ -38,19 +39,17 @@ const getRandomCard = function () {
 };
 
 // Функция создания карточки
-const createCard = function (index) {
-  const card = сardRandomizer ? getRandomCard() : initialCards[index];
-  return card;
-};
+const createCard = (index) =>
+  сardRandomizer ? getRandomCard() : initialCards[index];
 
 // Функция удаления карточки
 const removeCard = function (event) {
-  createdCards = createdCards.filter(function (card) {
-    return !(
-      card.name ===
-        event.target.parentElement.querySelector('.card__title').textContent &&
-      card.link === event.target.parentElement.querySelector('.card__image').src
-    );
+  createdCards = createdCards.filter(card => {
+    const targetName =
+      event.target.parentElement.querySelector('.card__title').textContent;
+    const targetLink =
+      event.target.parentElement.querySelector('.card__image').src;
+    return !(card.name === targetName && card.link === targetLink);
   });
   event.target.parentElement.remove();
 };
@@ -66,4 +65,7 @@ const initializeCards = function () {
 initializeCards();
 
 //Вывести карточки на страницу
-createdCards.forEach((card) => placesList.append(putCard(card, removeCard)));
+createdCards.forEach((card) =>
+  placesListClone.append(putCard(card, removeCard))
+);
+placesList.replaceWith(placesListClone);
